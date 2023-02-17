e32d09ab86dd08eb9bc67329816a3b1c4198fb66
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2023-02-16 22:24:24 -0300

    feat(config): modify eslint config and next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
