f0579a790ffb97732112805563eeb4a929411914
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2023-02-28 22:51:19 -0300

    adding download link

--- src/components/Buttons/PrimaryButton/index.tsx
+++ src/components/Buttons/PrimaryButton/index.tsx
@@ -4,8 +4,12 @@ import IPrimaryButtonProps from './types';
 import styles from './styles.module.scss';
 
 export default function PrimaryButton({ href, children, onClick, download }: IPrimaryButtonProps) {
-  return (
-    <Link href={href} className={styles.primaryButton} onClick={onClick} download={download}>
+  return download ? (
+    <a href={href} className={styles.primaryButton} download onClick={onClick}>
+      {children}
+    </a>
+  ) : (
+    <Link href={href} className={styles.primaryButton} onClick={onClick}>
       {children}
     </Link>
   );
