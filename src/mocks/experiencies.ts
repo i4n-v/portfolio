c4c0b720f6821598d9b96de240b7ea0c08b78f90
i4n-v <ianvinicioss@gmail.com>
2024-09-13 15:22:19 -0300

    feat: updating experiencies

--- src/mocks/experiencies.ts
+++ src/mocks/experiencies.ts
@@ -2,13 +2,19 @@ import { IExperiencie } from '@/models';
 
 const experiencies: IExperiencie[] = [
   {
-    name: 'Desenvolvedor Full Stack Junior',
+    name: 'Desenvolvedor Front-End & Mobile Pleno',
     period: 'Atual',
-    description: `Desenvolvimento e prototipação de interfaces, como também, consumo e implementação de APIs REST, utilizando Figma, React, React Native e Node.`,
-    tecnologies: ['Figma', 'React', 'React Native', 'Node'],
+    description: `Desenvolvimento, implementação de arquiteturas, padrões, implantação e prototipação de aplicações web e mobile, como também o gerenciamento e manutenção dos repositórios e da equipe de desenvolvimento, utilizando CI/CD com Gitlab/GCP.`,
+    tecnologies: ['Figma', 'React', 'Next', 'React Native', 'Node'],
   },
   {
-    name: 'Estagiário Front End',
+    name: 'Desenvolvedor Front-End & Mobile Junior',
+    period: '2022 - 2024',
+    description: `Desenvolvimento, implementação de arquiteturas, padrões, implantação e prototipação de aplicações web e mobile, como também o consumo de API’s REST, como também sua implementação e manutenção.`,
+    tecnologies: ['Figma', 'React', 'Next', 'React Native', 'Node'],
+  },
+  {
+    name: 'Estagiário Front-End',
     period: '2021 - 2022',
     description: `Implementação de interfaces e consumo de APIs REST utilizando HTML, CSS, JavaScript e React.`,
     tecnologies: ['React', 'HTML', 'CSS', 'JavaScript'],
